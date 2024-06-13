324b27188833f45c9674081f1b89989b468bd3cf
M3hdi-Rostami <m3hdi.rostami@gmail.com>
2024-06-13 14:00:38 +0330

    Style: refactor styles of header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,15 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="bg-white p-4 flex items-center justify-between border dir-ltr lg:px-[235px]">
-      <div className="flex items-center text-myblue">
+      <div className="flex items-center gap-4 text-myblue">
         <div className="flex items-center">
           <img src={Vector} alt=" " className="h-6 w-6 mr-2" />
-          <span className=" mr-4">EN|</span>
+          <span>EN|</span>
         </div>
-        <Link to="/about" className="mr-4 ">
-          درباره ما
-        </Link>
-        <Link to="/blog" className="mr-4">
-          بلاگ
-        </Link>
-        <Link to="/products" className="mr-4">
-          {" "}
-          محصولات
-        </Link>
-        <Link to="/" className="mr-4">
-          {" "}
-          خانه
-        </Link>
+        <Link to="/about">درباره ما</Link>
+        <Link to="/blog">بلاگ</Link>
+        <Link to="/products"> محصولات</Link>
+        <Link to="/"> خانه</Link>
       </div>
       <div className=" flex items-center">
         <span className=" font-medium text-myblue"> ایران ساین </span>{" "}

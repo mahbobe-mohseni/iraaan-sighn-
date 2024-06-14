cd0bf2048d02aacc1d117942606effe72c7d9af7
M3hdi-Rostami <m3hdi.rostami@gmail.com>
2024-06-14 14:58:27 +0330

    Refactor: refactor footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,68 +1,57 @@
-import React from 'react';
-import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaInstagram, FaTwitter, FaFacebook } from 'react-icons/fa';
+import React from "react";
+import {
+  FaPhoneAlt,
+  FaMapMarkerAlt,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-600 text-white py-8">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-between items-start">
-          {/* Left Section */}
-          <div className="flex flex-col items-center md:items-center  mb-6 md:mb-0">
-          <div className="flex flex-col items-center md:items-end">
-            <div className="mb-4 text-center md:text-right">
-              <p className="font-bold flex items-center justify-center md:justify-end">
-                <FaPhoneAlt className="ml-2" /> شماره تماس
-              </p>
-              <p>+98 123 456 789</p>
-              <p>شنبه تا چهارشنبه ساعت 8:00 الی 17:00</p>
-            </div>
-            <div className="mb-4 text-center md:text-right">
-              <p className="font-bold flex items-center justify-center md:justify-end">
-                <FaEnvelope className="ml-2" /> ایمیل
-              </p>
-              <p>info@iran-sign.ir</p>
-            </div>
-            <div className="mb-4 text-center md:text-right">
-              <p className="font-bold flex items-center justify-center md:justify-end">
-                <FaMapMarkerAlt className="ml-2" /> آدرس
-              </p>
-              <p>تهران، خیابان آزادی، بین خیابان نواب و اسکندری، پلاک 170، واحد 34</p>
-            </div>
+    <footer className="bg-blue-600 text-white py-8 px-32">
+      <div className="flex items-center justify-between lg:flex-row flex-col lg:gap-0 gap-6">
+        <div className="contact-box">
+          <div className="flex gap-2 mb-6">
+            <FaPhoneAlt /> <span>شماره تماس</span>
           </div>
-          <div className='grid grid-cols-2 gap-2 '>
-          <ul className="text-center md:text-left space-y-1">
-            <p className="text-center md:text-left font-bold mb-2">با ایران‌سلیمان</p>
-              <li>تماس با ما</li>
-              <li>قوانین و مقررات</li>
-              <li>پرسش متداول</li>
-              <li>سوالات متداول</li>
-            </ul>
+          <p className="text-12">+98 123 456 789</p>
+          <p className="text-12">شنبه تا چهارشنبه ساعت 8:00 الی 17:00</p>
+          <div className="flex gap-2 my-6">
+            <FaMapMarkerAlt /> <span>آدرس</span>
           </div>
-          
-            <div className="flex space-x-4 mt-4">
-              <FaInstagram className="text-white" />
-              <FaTwitter className="text-white" />
-              <FaFacebook className="text-white" />
-            </div>
-          </div>
-
-          {/* Right Section */}
-         
-
-
-
-          <div className="bg-white p-4 rounded-full mb-4">
-              <div className="grid grid-cols-2 gap-2">
-                <div className="w-8 h-8 bg-blue-600"></div>
-                <div className="w-8 h-8 bg-blue-600"></div>
-                <div className="w-8 h-8 bg-blue-600"></div>
-                <div className="w-8 h-8 bg-blue-600"></div>
-              </div>
-            </div>
+          <p className="text-12">
+            تهران، خیابان آزادی، بین خیابان نواب و اسکندری، پلاک 170، واحد 34
+          </p>
         </div>
-        <div className="text-center mt-6">
-          <p>تمام حقوق مادی و معنوی این سایت متعلق به ایران‌سلیمان می‌باشد</p>
+        <div className="links-box">
+          <p className="font-bold mb-6">با ایران ساین</p>
+          <div className="text-12 flex flex-col gap-4">
+            <Link to="/">تماس با ما</Link>
+            <Link to="/">قوانین و مقررات</Link>
+            <Link to="/">پرسش متداول</Link>
+            <Link to="/">سوالات متداول</Link>
+          </div>
         </div>
+        <div className="license-box">
+          <div className="grid grid-cols-2 gap-2">
+            <div className="w-16 h-16 bg-white rounded-lg"></div>
+            <div className="w-16 h-16 bg-white rounded-lg"></div>
+            <div className="w-16 h-16 bg-white rounded-lg"></div>
+            <div className="w-16 h-16 bg-white rounded-lg"></div>
+          </div>
+        </div>
+      </div>
+      <div className="social-box flex items-center justify-center my-6">
+        <div className="flex gap-6 w-max">
+          <FaInstagram className="text-white" />
+          <FaTwitter className="text-white" />
+          <FaLinkedin className="text-white" />
+        </div>
+      </div>
+      <div className="copyright-box text-center text-12">
+        <p>تمام حقوق مادی و معنوی این سایت متعلق به ایران ساین می‌باشد</p>
       </div>
     </footer>
   );

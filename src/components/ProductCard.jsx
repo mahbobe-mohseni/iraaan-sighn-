7600cd06179c7561e3e289ad8e9f6e8679a2c437
mahbobe-mohseni <mahbobe.mohsenee@gmail.com>
2024-06-24 09:31:40 -0400

    modify mainselectbox

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,3 @@
- 
-
- 
- 
 // import React from 'react';
 
 // const ProductCard = () => {
@@ -70,53 +66,81 @@ export default MyComponent;
 
 */
 
-
-import React from 'react';
-import { mahsol1,mahsol2,mahsol3,mahsol4,share,show,unsplash } from '../images';
-
-
-
-
-
+import React from "react";
+import {
+  mahsol1,
+  mahsol2,
+  mahsol3,
+  mahsol4,
+  share,
+  show,
+  unsplash,
+} from "../images";
 
 const images = [
-  { id: 1, imgUrl:  mahsol1 , title: 'خطی که مهمتر است را می‌توان در این قسمت نوشت', description: '   بخشی از اوایل مقاله به اندازه 2 خط اینجا نوشته شود، لورم ایپسوم متنی ساختگی است که در طراحی نگارش می‌شود...' },
-  { id: 2, imgUrl: mahsol2,title: 'خطی که مهمتر است را می‌توان در این قسمت نوشت  ', description: '   بخشی از اوایل مقاله به اندازه 2 خط اینجا نوشته شود، لورم ایپسوم متنی ساختگی است که در طراحی نگارش می‌شود...' },
-  { id: 3, imgUrl: mahsol3, title: 'خطی که مهمتر است را می‌توان در این قسمت نوشت  ', description: '   بخشی از اوایل مقاله به اندازه 2 خط اینجا نوشته شود، لورم ایپسوم متنی ساختگی است که در طراحی نگارش می‌شود...' }
+  {
+    id: 1,
+    imgUrl: mahsol1,
+    title: "خطی که مهمتر است را می‌توان در این قسمت نوشت",
+    description:
+      "   بخشی از اوایل مقاله به اندازه 2 خط اینجا نوشته شود، لورم ایپسوم متنی ساختگی است که در طراحی نگارش می‌شود...",
+  },
+  {
+    id: 2,
+    imgUrl: mahsol2,
+    title: "خطی که مهمتر است را می‌توان در این قسمت نوشت  ",
+    description:
+      "   بخشی از اوایل مقاله به اندازه 2 خط اینجا نوشته شود، لورم ایپسوم متنی ساختگی است که در طراحی نگارش می‌شود...",
+  },
+  {
+    id: 3,
+    imgUrl: mahsol3,
+    title: "خطی که مهمتر است را می‌توان در این قسمت نوشت  ",
+    description:
+      "   بخشی از اوایل مقاله به اندازه 2 خط اینجا نوشته شود، لورم ایپسوم متنی ساختگی است که در طراحی نگارش می‌شود...",
+  },
 ];
-
-
 
 const CustomCard = ({ imgUrl, title, description }) => {
   return (
-   <>
-   
-   <div className=" container flex flex-col md:flex-row items-center md:items-start gap-4 md:gap-8 w-full rounded overflow-hidden shadow-lg">
-      <img src={imgUrl} alt="Product" className="w-[427px] h-[318px] rounded-md" />
-      <div className="flex flex-col w-full md:w-1/2 p-4">
-        <h2 className="font-bold text-xl mb-2 text-gray-800">{title}</h2>
-        <p className="text-gray-700 text-base">{description}</p>
-        <div className="flex justify-between  gap-2 mt-4 text-10">
-        <div className='flex'>
-        <span className="inline-block  rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">15  خرداد</span>
-        <span className="inline-block   rounded-full px-3 py-1 text-sm font-semibold text-gray-700 pr-0 ">  | 5دقیقه </span>
-        </div>
-        {/* ***************************************************************************************ok nemishe  */}
-        <div className='flex items-center gap-2 '>
-          <div className='flex flex-row  gap-2'>
-          <span className="   gap-2 rounded-full  text-sm font-semibold text-gray-700">  اشتراک گذاری  <span><img src={share} className=' w-4 h-4' alt="" /></span></span>
+    <>
+      <div className=" container flex flex-col md:flex-row items-center md:items-start gap-4 md:gap-8 w-full rounded overflow-hidden shadow-lg">
+        <img
+          src={imgUrl}
+          alt="Product"
+          className="w-[427px] h-[318px] rounded-md"
+        />
+        <div className="flex flex-col w-full md:w-1/2 p-4">
+          <h2 className="font-bold text-xl mb-2 text-gray-800">{title}</h2>
+          <p className="text-gray-700 text-base">{description}</p>
+          <div className="flex justify-between  gap-2 mt-4 text-10">
+            <div className="flex  items-center f">
+              <span className="inline-block  rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                15 خرداد
+              </span>
+              <span className="inline-block   rounded-full px-3 py-1 text-sm font-semibold text-gray-700 pr-0 ">
+                {" "}
+                | 5دقیقه{" "}
+              </span>
+            </div>
+            <div
+              style={{ display: "grid", gridTemplateColumns: "repeat(2,1fr)" }}
+            >
+              <div style={{ display: "flex",paddingRight:10  }}>
+                <p className="   ">اشتراک گذاری</p>
 
-          </div>
-          <div className=' flex items-center gap-2'>
-          <span className="  rounded-full px-3 py-1 text-sm font-semibold text-gray-700"> <span><img src={share} className=' w-4 h-4' alt="" /></span>5585  </span>
+                <img src={share} className=" w-4 h-4" alt="" />
+              </div>
+              <div style={{ display: "flex" }}>
+                <p> 5585</p>
 
+                <img src={share} className=" w-4 h-4" alt="" />
+              </div>
+            </div>
           </div>
-          {/* ********************************************************************************************** */}
-        </div>
         </div>
       </div>
-    </div>
-   </>
+    </>
   );
 };
 
@@ -124,12 +148,15 @@ const App = () => {
   return (
     <div className="flex flex-col items-center gap-4">
       {images.map(({ id, imgUrl, title, description }) => (
-        <CustomCard key={id} imgUrl={imgUrl} title={title} description={description} />
+        <CustomCard
+          key={id}
+          imgUrl={imgUrl}
+          title={title}
+          description={description}
+        />
       ))}
     </div>
   );
 };
 
 export default App;
- 
- 
